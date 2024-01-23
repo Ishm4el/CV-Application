d250849fb075041358973d63fa4c23fff6fd5b66
Ishii <ismaelv@protonmail.com>
2024-01-23 03:26:42 -0800

    Experience Section can now have various entries

--- src/components/Practical.jsx
+++ src/components/Practical.jsx
@@ -1,6 +1,29 @@
-export default function Practical({ change }) {
+import { useState, Fragment } from "react";
+
+export default function Practical() {
+   const [entries, setEntries] = useState({});
+   function addEntry() {
+      const newID = crypto.randomUUID();
+      const newEntry = entry(newID);
+      setEntries({ ...entries, [newID]: newEntry });
+   }
    return (
       <>
+         <button type="button" onClick={addEntry}>
+            Add Experience Entry
+         </button>
+         {Object.keys(entries).map((value) => (
+            <div className="container-form-section-sub" key={value}>
+               {entries[value]}
+            </div>
+         ))}
+      </>
+   );
+}
+
+function entry(id) {
+   return (
+      <Fragment key={id}>
          <label htmlFor="companyName">Company Name:</label>
          <input type="text" name="companyName" id="companyName" />
          <label htmlFor="positionTitle">Position Title:</label>
@@ -9,6 +32,6 @@ export default function Practical({ change }) {
          <input type="text" name="responsibility" id="responsibility" />
          <label htmlFor="dateWork">Date Worked</label>
          <input type="datetime" name="dateWork" id="dateWork" />
-      </>
+      </Fragment>
    );
 }
